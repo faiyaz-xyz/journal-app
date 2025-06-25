2245b194afbfd87b379c1c04a4d2cb58ea4dba28
Ayman Tahiat Faiyaz <tahiat0967@gmail.com>
2025-06-25 08:52:17 +0600

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="w-64 h-40 bg-pink-200 border-2 border-black rounded-lg shadow-[4px_4px_0_0_rgba(0,0,0,1)] p-4 select-none pixel-font">
+        <h2 className="text-black text-xl mb-2">Pixelated Box</h2>
+        <p className="text-black">This is a cute pixel-style round box with a shadow and pixel font.</p>
+      </div>
+    </div>
+  );
+}
